ffb395230d123ff9a9041347afe43ae13c547991
Brian Jesse <brian.jesse@gmail.com>
2019-02-23 00:14:13 -0600

    docs(light): add light example files

--- site/light.vue
+++ site/light.vue
@@ -1,5 +1,28 @@
+<script>
+import Example from '../examples/light.vue';
+import Controls from './controls.vue';
+
+export default {
+  components: {
+    Example,
+    Controls,
+  },
+};
+</script>
+
 <template lang="pug">
 article
   h2 Light
+
+  h4 Example
+
+  Controls(:inline="true" route="/light/fullscreen")
+    Example
+    h5(slot="filename") light.vue
+    pre(slot="pug")
+      include:hl(lang="pug") ../examples/light.pug
+    pre(slot="html")
+      include:hl:pug(pretty=true) ../examples/light.pug
+
   include:markdown-it(plugins=['markdown-it-prism']) ../src/light/docs.md
 </template>
