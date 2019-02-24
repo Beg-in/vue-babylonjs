c6e137495c543df1c1d38961114e688ef79c19f3
Linda Chen <lchen107@hawk.iit.edu>
2019-02-23 22:36:19 -0600

    add entity example

--- site/entity.vue
+++ site/entity.vue
@@ -1,5 +1,28 @@
+<script>
+import Example from '../examples/entity.vue';
+import Controls from './controls.vue';
+
+export default {
+  components: {
+    Example,
+    Controls,
+  },
+};
+</script>
+
 <template lang="pug">
 article
   h2 Entity
+
+  h4 Example
+
+  Controls(:inline="true" route="/entity/fullscreen")
+    Example
+    h5(slot="filename") entity.vue
+    pre(slot="pug")
+      include:hl(lang="pug") ../examples/entity.pug
+    pre(slot="html")
+      include:hl:pug(pretty=true) ../examples/entity.pug
+
   include:markdown-it(plugins=['markdown-it-prism']) ../src/entity/docs.md
 </template>
