cf40ca9af79c13d0a7575e178e925967bd00edb6
Brian Jesse <brian.jesse@gmail.com>
2019-03-03 23:14:10 -0600

    docs(texture): add example

--- site/texture.vue
+++ site/texture.vue
@@ -1,5 +1,28 @@
+<script>
+import Example from '../examples/texture.vue';
+import Controls from './controls.vue';
+
+export default {
+  components: {
+    Example,
+    Controls,
+  },
+};
+</script>
+
 <template lang="pug">
 article
   h2 Texture
+
+  h4 Example
+
+  Controls(:inline="true" route="/animations/fullscreen")
+    Example
+    h5(slot="filename") texture.vue
+    pre(slot="pug")
+      include:hl(lang="pug") ../examples/texture.pug
+    pre(slot="html")
+      include:hl:pug(pretty=true) ../examples/texture.pug
+
   include:markdown-it(plugins=['markdown-it-prism']) ../src/texture/docs.md
 </template>
